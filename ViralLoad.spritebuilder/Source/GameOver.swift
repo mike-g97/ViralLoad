d08c3b9dff245f448246db3f692fd36ae5b335ca
Mikhael E Gonzalez <mikhael.gonzalez@gmail.com>
2015-07-15 13:43:34 -0400

    Finished basic Game Over and restart.

---
+++ ViralLoad.spritebuilder/Source/GameOver.swift
@@ -0,0 +1,13 @@
+//
+//  GameOver.swift
+//  ViralLoad
+//
+//  Created by Mikhael Gonzalez on 7/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class GameOver: CCNode {
+   
+}
---
+++ ViralLoad.spritebuilder/Source/Singleton.swift
@@ -0,0 +1,13 @@
+//
+//  Singleton.swift
+//  ViralLoad
+//
+//  Created by Mikhael Gonzalez on 7/15/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Singleton: NSObject {
+   
+}
